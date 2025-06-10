39fcb1c1fbc69e428ae0317535c6c7c963511b1d
CDevs <constantsuchet@gmail.com>
2025-06-10 18:07:26 +0200

    changes

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     id: '1',
     title: 'ChronoSync',
     description: 'A modern time management app tool built for developers. Designed to help you focus on what matters most.',
-    image: '/images/project1.png',
+    image: '/images/Chronosync.png',
     tags: ['Nuxt.Js', 'Vue.Js', 'Tailwind CSS', 'C++ Application', 'Express.Js', 'PostgreSQL'],
     link: 'https://example.com',
     featured: true,
@@ -23,27 +23,27 @@ export const projects: Project[] = [
   },
   {
     id: '2',
-    title: 'Wayfinding System',
-    description: 'Modern wayfinding and signage system design for educational institutions. Clean, accessible design with clear visual hierarchy.',
-    image: '/images/project2.jpg',
-    tags: ['Wayfinding', 'Signage', 'UX Design'],
+    title: 'Roblox Inventory',
+    description: 'Inventory System for games featuring Drag&Drop, item size system, hotbar etc (inspired of Tarkov inventory).',
+    image: '/images/InventoryRoblox.png',
+    tags: ['Roblox', 'Lua', 'UI/UX'],
     featured: true,
     size: 'medium'
   },
   {
     id: '3',
-    title: 'Creative Project 3',
-    description: 'Description for your third project. Easy to customize and modify.',
-    image: '/images/project3.jpg',
-    tags: ['Design', 'Creative'],
-    size: 'small'
+    title: 'Pauvocoder',
+    description: 'Simplified implementation of a vocoder allowing audio signal manipulation, specifically focused on pitch shifting while preserving the speech tempo.',
+    image: '/images/Pauvocoder.png',
+    tags: ['Java'],
+    size: 'medium'
   },
   {
     id: '4',
-    title: 'Creative Project 4',
-    description: 'Description for your fourth project. Easy to customize and modify.',
-    image: '/images/project4.jpg',
-    tags: ['Design', 'Creative'],
-    size: 'medium'
+    title: 'PuissanceX',
+    description: 'A modified version of Connect 4 with X win condition created in Java with the framework Boardifier.',
+    image: '/images/PuissanceX.png',
+    tags: ['Java', 'JavaFX', 'JUnit', 'Maven'],
+    size: 'large'
   }
 ];
